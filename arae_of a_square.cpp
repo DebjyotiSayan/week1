ffb90e26c34487ecf68b5d8e2064633d877614b1
DebjyotiSayan <143388775+DebjyotiSayan@users.noreply.github.com>
2023-10-02 22:57:02 +0530

    Add files via upload

---
+++ arae_of a_square.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int a,b;
+
+int main(){
+	printf("enter the lenght of the side:");
+	scanf("%d",&a);
+	b=pow(a,2);
+	printf("The area of the square is=%d",b);
+	return 0;
+}
